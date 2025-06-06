02cbb1a758fc69c660e97e7dd72358a27f0c16bf
Yevhen Petrun <petrun.yevhen@student.uzhnu.edu.ua>
2025-06-06 23:10:31 +0300

    modified

--- src/API/HotelBooking.API/Program.cs
+++ src/API/HotelBooking.API/Program.cs
@@ -1,41 +1,26 @@
+using Booking.Infrastructure.Data;
+using Booking.Infrastructure.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+builder.Services.AddBookingModule();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<BookingDbContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("ConnectionString"));
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast");
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
