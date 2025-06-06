73c27e6b4064b5725996877b9638ca5bf85af4b2
Yevhen Petrun <petrun.yevhen@student.uzhnu.edu.ua>
2025-06-06 23:08:21 +0300

    add initial methods

---
+++ src/API/HotelBooking.API/Controllers/BookingController.cs
@@ -0,0 +1,31 @@
+﻿using Application.Command;
+using Application.Query;
+using Booking.Domain.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelBooking.API.Controllers;
+
+[ApiController]
+[Route("booking")]
+public class BookingController : ControllerBase
+{
+    private readonly IMediator _mediator;
+    
+    public BookingController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+    
+    [HttpGet("{reservationId}")]
+    public async Task<Reservation> GetReservationById(Guid  reservationId)
+    {
+        return await _mediator.Send(new GetReservationByIdQuery(new ReservationId(reservationId)));
+    }
+    
+    [HttpPost("add-reservation")]
+    public async Task<Reservation> AddReservation(AddReservationCommand request)
+    {
+        return await _mediator.Send(request);
+    }
+}
