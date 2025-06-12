bfadbcdd5281f2638d1d9dcb5ef642b017d6610d
Yevhen Petrun <petrun.yevhen@student.uzhnu.edu.ua>
2025-06-12 16:26:08 +0300

    add new Hotel controller

---
+++ src/API/HotelBooking.API/Controllers/HotelController.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using RoomManagment.Application.Command.AddRoom;
+using RoomManagment.Domain.Entities;
+
+namespace HotelBooking.API.Controllers;
+
+[ApiController]
+[Route("hotel")]
+public class HotelController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public HotelController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+    
+    [HttpPost("add-room")]
+    public async Task<Room> AddRoom(AddRoomCommand request)
+    {
+        if (request == null) throw new ArgumentNullException(nameof(request));
+        
+        return await _mediator.Send(request);
+    }
+}
