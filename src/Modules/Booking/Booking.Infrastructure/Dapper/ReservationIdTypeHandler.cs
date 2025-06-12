ed983129c1357abef59c994e5ce5456a5444a3d2
Yevhen Petrun <petrun.yevhen@student.uzhnu.edu.ua>
2025-06-12 16:22:06 +0300

    add type handler for dapper, DI, Write-Read repositories

---
+++ src/Modules/Booking/Booking.Infrastructure/Dapper/ReservationIdTypeHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.Data;
+using Booking.Domain.Entities;
+using Dapper;
+
+namespace Booking.Infrastructure.Dapper;
+
+public class ReservationIdTypeHandler : SqlMapper.TypeHandler<ReservationId>
+{
+    public override void SetValue(IDbDataParameter parameter, ReservationId? value) 
+        => parameter.Value = value?.Value;
+
+    public override ReservationId? Parse(object value)
+    => new ReservationId((Guid)value);
+}
