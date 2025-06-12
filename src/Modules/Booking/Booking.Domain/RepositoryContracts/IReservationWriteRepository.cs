f64ff4a60efc6668f54852489e14df6fdfe0197e
Yevhen Petrun <petrun.yevhen@student.uzhnu.edu.ua>
2025-06-12 16:22:55 +0300

    add new method for repository

---
+++ src/Modules/Booking/Booking.Domain/RepositoryContracts/IReservationWriteRepository.cs
@@ -0,0 +1,8 @@
+﻿using Booking.Domain.Entities;
+
+namespace Booking.Domain.RepositoryContracts;
+
+public interface IReservationWriteRepository 
+{
+    Task AddReservationAsync(Reservation reservation, CancellationToken cancellationToken);
+}
